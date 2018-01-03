a3942475d5298ce8a4d8f8b0c7e973db4829001a
Ryan Jarrett <ryan@sparkdevelopment.co.uk>
2018-01-03 20:46:09 +0000

    Recompile assets

---
+++ wp-content/themes/justtravelmoney/dist/scripts/ajax-87b7388c5c.js
@@ -0,0 +1,1 @@
+!function(e){e("#jtm_currency-update").on("click",function(r){r.preventDefault(),$trigger_container=e(this).parent();var t={action:"update_currencies",refresh:"false"};$trigger_container.html("<em>Updating...</em>"),jQuery.post(ajaxurl,t,function(e){$trigger_container.html("<em>Currency tables updated</em>"),console.log(e)})})}(jQuery);
