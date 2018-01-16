f6bb8ce5eb1db07bd04e5cb44ecab3cb2f3617df
Ryan Jarrett <ryan@sparkdevelopment.co.uk>
2018-01-16 08:29:59 +0000

    Add pre-fold currencies and open links in new tab

--- wp-content/themes/justtravelmoney/templates/content-single-providers-table.php
+++ wp-content/themes/justtravelmoney/templates/content-single-providers-table.php
@@ -1,6 +1,6 @@
 <?php
 $currencies = new \jtm\Currencies();
-$exchange_results = $currencies->provider(get_the_id(), true);
+$exchange_results = $currencies->provider( get_the_id(), true );
 $hero_title = get_the_title();
 $hero_title_meta = rwmb_meta( 'WDC_provider_hero_title', array(), get_the_id() );
 //Meta
@@ -8,56 +8,64 @@ $website_url = rwmb_meta( 'WDC_about_provider_url', array(), get_the_id() );
 $affiliate_url = rwmb_meta( 'WDC_about_provider_affiliate_url', array(), get_the_id() );
 
 $buy_now_url = $website_url;
-if($affiliate_url){
-    $buy_now_url = $affiliate_url;
+if ( $affiliate_url ) {
+	$buy_now_url = $affiliate_url;
 }
 
-if($hero_title_meta){
-    $hero_title = $hero_title_meta;
+if ( $hero_title_meta ) {
+	$hero_title = $hero_title_meta;
 }
 if($exchange_results): ?>
 
 <div class="compare-rates">
 
-    <header>
-    
-        <h3><?php echo sprintf( __('Latest %s Rates', 'sage'), $hero_title); ?></h3>
-        <span class="update_time">(59:43 Until Next Update)</span>
-    </header>
+	<header>
+	
+		<h3><?php echo sprintf( __('Latest %s Rates', 'sage'), $hero_title); ?></h3>
+		<span class="update_time">(59:43 Until Next Update)</span>
+	</header>
 
-    <table id="currency-comparison-list" class="table">
-        <tr class="nobg">
-            <th class="currency"><?php _e('Currency', 'sage');?></th>
-            <th class="exchange-rate"><?php _e('<span class="no-mobile">Exchange </span>Rate', 'sage');?></th>
-            <th class="you-get"><?php _e('&pound; 100 Buys', 'sage');?></th>
-            <th class="action"></th>
-        </tr>
+	<table id="currency-comparison-list" class="table">
+		<tr class="nobg">
+			<th class="currency"><?php _e('Currency', 'sage');?></th>
+			<th class="exchange-rate"><?php _e('<span class="no-mobile">Exchange </span>Rate', 'sage');?></th>
+			<th class="you-get"><?php _e('&pound; 100 Buys', 'sage');?></th>
+			<th class="action"></th>
+		</tr>
 
-        <?php
-        $i = 0;
-        foreach($exchange_results as $name => $exchange){ 
-            $i++;
-            $currency_name = $name;
-            $exchange_int = floatval($exchange);
-            $hundred_pounds_buys = 100 * $exchange_int;
-            $currency_plural = \Roots\Sage\Setup\jtm_get_currency_name($currency_name);
-            $row_class = 'tr';
-            if($i > 6){
-                $row_class = 'tr noshow';
-            }
-        ?>
+		<?php
+		// Prepare order of rates
+		ksort( $exchange_results );
+		$prefold_currencies = array( 'USD','EUR','TRY','THB','CHF','AED' );
+		foreach ( array_reverse( $prefold_currencies ) as $prefold_currency ) {
+			$exchange_results = array( $prefold_currency => $exchange_results[$prefold_currency] ) + $exchange_results;
+		}
 
-        <tr class="<?php echo $row_class;?>">
-            <td class="currency"><div><span class="name"><?php echo $currency_plural;?></span> <span class="iso"><?php echo $currency_name;?></span></div></td>
-            <td class="exchange-rate"><div><?php echo $exchange_int;?></div></td>
-            <td class="you-get"><div><?php echo $hundred_pounds_buys.' <span class="iso">('.$currency_name.')</span>';?></div></td>
-            <td class="action"><a href="<?php echo $buy_now_url;?>" class="btn btn-secundary"><?php _e('Buy Now', 'sage');?></a></td>
-        </tr>
-        
-        <?php } ?>
-    </table>
-    <div class="show-all-results">
-        <a href="#" id="show-all-exchange-results"><?php _e('View All Rates', 'sage');?></a>
-    </div>
+		$i = 0;
+		foreach($exchange_results as $name => $exchange){ 
+			$i++;
+			$currency_name = $name;
+			$exchange_int = floatval($exchange);
+			$hundred_pounds_buys = 100 * $exchange_int;
+			$currency_plural = \Roots\Sage\Setup\jtm_get_currency_name($currency_name);
+			$row_class = 'tr';
+			if($i > 6){
+				$row_class = 'tr noshow';
+			}
+		?>
+
+		<tr class="<?php echo $row_class;?>">
+			<td class="currency"><div><span class="name"><?php echo $currency_plural;?></span> <span class="iso"><?php echo $currency_name;?></span></div></td>
+			<td class="exchange-rate"><div><?php echo $exchange_int;?></div></td>
+			<td class="you-get"><div><?php echo $hundred_pounds_buys.' <span class="iso">('.$currency_name.')</span>';?></div></td>
+			<td class="action"><a href="<?php echo $buy_now_url;?>" class="btn btn-secundary" target="_blank"><?php _e('Buy Now', 'sage');?></a></td>
+		</tr>
+		
+		<?php } ?>
+	</table>
+	<div class="show-all-results">
+		<a href="#" id="show-all-exchange-results"><?php esc_html_e('View All Rates', 'sage');?></a>
+	</div>
 </div>
-<?php endif; ?>
+<?php
+endif;
