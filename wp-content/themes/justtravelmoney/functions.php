968bcd857ce5292b79e707c81eac44453a4d3e84
Ryan Jarrett <ryan@sparkdevelopment.co.uk>
2018-02-04 22:36:29 +0000

    Add custom permalink rewrite for blog archive

--- wp-content/themes/justtravelmoney/functions.php
+++ wp-content/themes/justtravelmoney/functions.php
@@ -10,25 +10,26 @@
  * @link https://github.com/roots/sage/pull/1042
  */
 $sage_includes = [
-	'lib/assets.php',           // Scripts and stylesheets
-	'lib/extras.php',           // Custom functions
-	'lib/setup.php',            // Theme setup
-	'lib/titles.php',           // Page titles
-	'lib/wrapper.php',          // Theme wrapper class
-	'lib/customizer.php',       // Theme customizer
-	'lib/misc.php',             // Misc Functions
-	'lib/homepage-meta.php',    // Homepage Metabox
-	'lib/contact-meta.php',     // Contact Metabox
-	'lib/general-meta.php',     // Homepage Metabox
-	'lib/shortcodes.php',       // Custom Shortcodes
-	'lib/cpt.php',              // Custom Post Types
-	'lib/cpt-permalinks.php',   // Remove CPT Permalink
-	'lib/widgets.php',          // Custom Widgets
-	'lib/pagination.php',       // Custom Pagination
-	'lib/import-meta.php',      // Import Metabox
-	'lib/class-currencies.php', // Import class
-	'lib/class-ajax.php',       // AJAX class
-	'lib/class-cron.php',       // Cron class
+	'lib/assets.php',            // Scripts and stylesheets
+	'lib/extras.php',            // Custom functions
+	'lib/setup.php',             // Theme setup
+	'lib/titles.php',            // Page titles
+	'lib/wrapper.php',           // Theme wrapper class
+	'lib/customizer.php',        // Theme customizer
+	'lib/misc.php',              // Misc Functions
+	'lib/homepage-meta.php',     // Homepage Metabox
+	'lib/contact-meta.php',      // Contact Metabox
+	'lib/general-meta.php',      // Homepage Metabox
+	'lib/shortcodes.php',        // Custom Shortcodes
+	'lib/cpt.php',               // Custom Post Types
+	'lib/cpt-permalinks.php',    // Remove CPT Permalink
+	'lib/widgets.php',           // Custom Widgets
+	'lib/pagination.php',        // Custom Pagination
+	'lib/import-meta.php',       // Import Metabox
+	'lib/class-currencies.php',  // Import class
+	'lib/class-ajax.php',        // AJAX class
+	'lib/class-cron.php',        // Cron class
+	'lib/class-seo-rewrite.php', // Cron class
 ];
 
 foreach ( $sage_includes as $file ) {
